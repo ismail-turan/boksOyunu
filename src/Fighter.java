71fed80b859b9a460288feb9dc9c06e0f9965012
Ismail <natdratuis@gmail.com>
2022-12-26 23:58:14 +0300

    Initial commit

---
+++ src/Fighter.java
@@ -0,0 +1,35 @@
+public class Fighter {
+    String name;
+    int damage;
+    int health;
+    int weight;
+    int dodge;
+    Fighter(String name,int damage,int health,int weight,int dodge){
+        this.name=name;
+        this.damage=damage;
+        this.health=health;
+        this.weight=weight;
+        if (dodge>=0 && dodge<=0){
+            this.dodge=dodge;
+        }else {
+            this.dodge=0;
+        }
+    }
+    int hit(Fighter foe){
+        System.out.println(this.name+" => "+foe.name+" "+this.damage+" hasar vurdu.");
+        if (isDodge()){
+            System.out.println(foe.name+"gelen hasarı blokladı!");
+            System.out.println("--------------------------------");
+            return  foe.health;
+        }
+        if (foe.health-this.damage<0){
+            return 0;
+        }
+       return foe.health-this.damage;
+    }
+    boolean isDodge(){
+        double randomNumber=Math.random()*100;
+        return randomNumber<=this.dodge;
+
+    }
+}
